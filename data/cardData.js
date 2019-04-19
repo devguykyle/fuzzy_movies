1b0ed03906fbceb4ce6b91fcc34cf90a51f9c23d
Christopher D. Fernandez <cdfernandez@mkbkpro-2G8WM.fios-router.home>
2019-04-18 22:21:12 -0700

    adding dummy data

--- data/cardData.js
+++ data/cardData.js
@@ -1,5 +1,6 @@
  module.exports =
  [
    {"title": "Card 1", "details": "These are Details for card 1"},
-   {"title": "Card 2", "details": "These are Details for card 2"}
+   {"title": "Card 2", "details": "These are Details for card 2"},
+   {"title": "Card 3", "details": "These are Details for card 3"}
 ]
